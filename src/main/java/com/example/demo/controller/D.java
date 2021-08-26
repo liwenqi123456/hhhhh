17547b4ecb101bb71393fe6800a5baf581140c1e
li <1106421164@qq.com>
2021-08-26 23:58:24 +0800

    测试1

---
+++ src/main/java/com/example/demo/controller/D.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+public class D {
+    private int c;
+    private int d;
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+    public int rrr(){
+        return  c+d;
+    }
+}
