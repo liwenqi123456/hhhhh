3b39b85ee31bd497358a9edd406732566e12934a
li <1106421164@qq.com>
2021-09-21 23:57:08 +0800

    测试1

--- src/main/java/com/example/demo/controller/A.java
+++ src/main/java/com/example/demo/controller/A.java
@@ -2,8 +2,8 @@ package com.example.demo.controller;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("13");
-        System.out.println("15");
+        System.out.println("rrr");
+        System.out.println("rrrr");
 
     }
 }
