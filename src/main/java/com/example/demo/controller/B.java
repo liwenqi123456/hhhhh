55a4377f2110b7fb8e1cd85816f043aa9fea0810
li <1106421164@qq.com>
2021-10-16 00:17:44 +0800

    yy

--- src/main/java/com/example/demo/controller/B.java
+++ src/main/java/com/example/demo/controller/B.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 public class B {
     private  int aa;
-    private int aa;
     private int ccc;
     private int ddd;
     private int rrr;
     private int sss;
+    public void bbb(){
+        System.out.println("44444");
+    }
 }
